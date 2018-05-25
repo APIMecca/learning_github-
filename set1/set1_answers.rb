af9d76c76c99d8b531c2a4128cd5e340b6af6c53
Mecca Gonzalez <meccaofseo@gmail.com>
2018-05-25 14:04:38 -0400

    Set 1 answers are done.

---
+++ set1/set1_answers.rb
@@ -0,0 +1,46 @@
+=begin
+
+# 1.1
+
+"Mecca " + "Gonzalez"
+
+# 1.2
+
+thousands = 4936 / 1000
+hundreds = 4936 % 1000 / 100
+tens = 4936 % 1000 % 100 / 10
+ones = 4936 % 1000 % 100 % 10
+
+# 1.3
+
+movies = {:Mecca => 1996, :Selenya => 2001, :Troy => 1962, :Brenda => 1972}
+
+puts movies{:Mecca}
+puts movies{:Selenya}
+puts movies{:Troy}
+puts movies{:Brenda}
+
+# 1.4
+
+arr = [1996, 2001, 1962, 1972]
+
+puts arr[0]
+puts arr[1]
+puts arr[2]
+puts arr[3]
+
+# 1.5
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# 1.6
+
+puts 4.568 * 4.568
+puts 7.697 * 7.697
+puts 3.980 * 3.980
+
+# 1.7
+# suppose to be a curly bracket, not a parenthesis 
